55003474600fdd9fbf37eff4fd90948cc1befc39
Anushka Agrahari <anushkaagrahari17@gmail.com>
2026-09-03 15:39:44 +0530

    Time: 2 ms (99.35%), Space: 47.2 MB (23.87%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,14 +1,3 @@
-// class Solution {
-//     public int[] twoSum(int[] nums, int target) {
-//         for(int i = 0;i<=nums.length;i++){
-//             int sum = nums[i] + nums[i+1];
-//             if (sum == target){
-//                 return num[i],nums[i+1];
-//             }
-//         }
-//     }
-// }
-
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
@@ -24,23 +13,5 @@ class Solution {
         }
 
         return new int[]{};
-           
-        //    Arrays.sort(nums);
-
-        //    int i=0,j=nums.length-1;
-        //    while(i<j){
-        //     int sum = nums[i]+nums[j];
-
-        //     if(sum==target){
-        //         return new int[]{i,j};
-        //     }
-        //     else if(sum< target){
-        //         i++;
-        //     }
-        //     else{
-        //         j--;
-        //    }
-        //    }
-        //    return new int[]{-1,-1};
-           }
     }
+}
